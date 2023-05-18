8e4fca916dc48e69c9ac9f6465e86c35a22eb426
rosemaryto <rosemary_to@ymail.com>
2023-05-17 18:02:44 -0700

    init

---
+++ js/oop-practice.js
@@ -0,0 +1,82 @@
+// STEP 1
+function Cat() {}
+const Dog = function () {}
+
+// STEP 2
+const firstCat = new Cat()
+const firstDog = new Dog()
+
+// STEP 3
+// class Animal {
+//     constructor() {
+//         console.log(`The animal has been created.`)
+//     }
+// }
+// const firstAnimal = new Animal()
+
+// STEP 4
+// class Animal {
+//     constructor(species, color) {
+//         this.species = species
+//         this.color = color
+//         console.log(`The ${this.color} ${this.species} has been created.`)
+//     }
+// }
+// const firstAnimal = new Animal('Hummingbird','Blue')
+
+// STEP 5
+// class Animal {
+//     constructor(type, breed, color, height, length) {
+//         this.type = type
+//         this.breed = breed
+//         this.color = color
+//         this.height = height
+//         this.length = length
+//         console.log(`Type: ${this.type} 
+//         Breed: ${this.breed}
+//         Color: ${this.color}
+//         Height: ${this.height}
+//         Length: ${this.length}
+//         The new animal has been created.`)
+//     }
+// }
+// const firstAnimal = new Animal('Bird','Hummingbird','Blue','3','6')
+
+// STEP 6
+// class Animal {
+//     constructor(type, breed, color, height, length) {
+//         this.type = type
+//         this.breed = breed
+//         this.color = color
+//         this.height = height
+//         this.length = length
+//     }
+
+// }
+// const firstAnimal = new Animal('Bird','Hummingbird','Blue','3','6')
+
+// for (const prop in firstAnimal) {
+//     console.log(`${prop}`)
+// }
+
+// STEP 7
+class Animal {
+    constructor(breed) {
+        this.breed = breed
+    }
+    speak = () => {
+        if (this.breed == 'dog') {
+            console.log(`${this.breed} has barked!`)
+        } else {
+            console.log(`${this.breed} has meowed!`)
+        }
+    }
+}
+const firstAnimal = new Animal('cat')
+firstAnimal.speak()
+
+// STEP 8
+
+
+
+// STEP 9
